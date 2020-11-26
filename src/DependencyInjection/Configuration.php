c2a475318fa3c5e439bb912e8caa1e5025aadfa7
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-11-26 21:34:49 +0100

    first commit

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Notify\Symfony\SweetAlert\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('notify_sweet-alert');
+
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('notify_sweet_alert');
+        }
+
+        $rootNode
+            ->children()
+                ->arrayNode('scripts')
+                    ->prototype('scalar')->end()
+                    ->defaultValue(array(
+                        'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+                        'https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.10.2/sweetalert2.min.js',
+                        'https://cdnjs.cloudflare.com/ajax/libs/promise-polyfill/8.2.0/polyfill.min.js',
+                    ))
+                ->end()
+                ->arrayNode('styles')
+                    ->prototype('scalar')->end()
+                    ->defaultValue(array(
+                        'https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.10.2/sweetalert2.min.css',
+                    ))
+                ->end()
+                ->arrayNode('options')
+                    ->ignoreExtraKeys(false)
+                    ->prototype('variable')->end()
+                    ->defaultValue(array(
+                        'timer'             => 10000,
+//                        'width'             => '32rem',
+//                        'heightAuto'        => true,
+                        'padding'           => '1.25rem',
+                        'showConfirmButton' => false,
+                        'showCloseButton'   => false,
+                        'toast'             => true,
+                        'position'          => 'top-end',
+                        'timerProgressBar'  => true,
+                    ))
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ src/NotifySweetAlertBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Notify\Symfony\SweetAlert;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NotifySweetAlertBundle extends Bundle
+{
+}
